2f96566d17b60ec936411989c9d441b733697e0c
Steven Hunal <stevehunal@gmail.com>
2019-06-30 16:51:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,11 @@
 def using_push(array, string)
-
 array = ["red", "orange", "yellow", "green", "blue", "indigo"]
 string = "violet"
 array.push(string)
 end
 
+def using_unshift (array,string)
+  array = ["small", "tall", "large"]
+  string = "big"
+  array.unshift(string)
+end
